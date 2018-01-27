463414b330d84f45e0dd1776c7e011e8125f86f7
Zhiping Wu <zpwu@thoughtworks.com>
2018-01-27 22:51:40 +0800

    add author spider

--- crawlers/crawlers/spiders/quotes_spider.py
+++ crawlers/crawlers/spiders/quotes_spider.py
@@ -8,7 +8,11 @@ class QuotesSpider(scrapy.Spider):
             'http://quotes.toscrape.com/page/1/',
             'http://quotes.toscrape.com/page/2/']
         
+        tag = getattr(self, 'tag', None)
+
         for url in urls:
+            if tag is not None:
+                url = url + 'tag/' + tag
             yield scrapy.Request(url=url, callable=self.parse)
 
     def parse(self, response):
@@ -18,3 +22,6 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').extract_first(),
                 'tags': quote.css('div.tags a.tag::text').extract(),
             }
+        
+        for a in response.css('li.next a'):
+            yield response.follow(a, callback=self.parse)
