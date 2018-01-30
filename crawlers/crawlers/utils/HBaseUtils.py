8362a41ac730ad6cf4b06d24da24824f2322eed5
Zhiping Wu <zpwu@thoughtworks.com>
2018-01-30 21:42:17 +0800

    update HBase tool

--- crawlers/crawlers/utils/HBaseUtils.py
+++ crawlers/crawlers/utils/HBaseUtils.py
@@ -5,24 +5,23 @@ from happybase import Batch
 
 import happybase as hb
 
-TABLE_NAME = "TABLE_NAME"
-
 class HBaseConnector:
-    def __init__(self, host, table_prefix):
+    def __init__(self, host, table_prefix, table_name):
         self.pool = hb.ConnectionPool(
             size = 16,
             host = host,
             autoconnect = True,
             table_prefix = table_prefix
         )
+        self.table_name = table_name
     
     def insert_data(self, datas):
         with self.pool.connection() as connection:
             tables = connection.tables()
             if TABLE_NAME not in tables:
-                connection.create_table(TABLE_NAME, families={'page': dict()})
+                connection.create_table(self.table_name, families={'page': dict()})
             
-            table = connection.table(TABLE_NAME)
+            table = connection.table(self.table_name)
             current_timestamp = int(round(time.time() * 1000))
             with Batch(table, batch_size=1000, timestamp=current_timestamp) as batch:
                 for data in datas:
@@ -36,7 +35,7 @@ class HBaseConnector:
 
     def scan_data(self):
         with self.pool.connection() as connection:
-            table = connection.table(TABLE_NAME)
+            table = connection.table(self.table_name)
             for key, data in table.scan():
                 yield key, data
 
